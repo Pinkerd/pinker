6a333931f107c9a7d7f314ecef4f5335cfd4b331
yt6666 <821596175@qq.com>
2017-12-26 19:20:25 +0800

    tao

---
+++ src/com/pinker/service/BlogDaoService.java
@@ -0,0 +1,29 @@
+package com.pinker.service;
+
+
+import com.pinker.entity.Blog;
+
+import java.util.List;
+
+public interface BlogDaoService {
+    /**
+     * 增加一条数据
+     */
+    int SaveBlog(Blog blog);
+    /**
+     *查询所有数据
+     */
+    List<Blog> getAllBlog();
+    /**
+     * 查询一条数据
+     */
+    Blog getBlogById(Integer id);
+    /**
+     * 删除一条数据
+     */
+    int DelBlog(Integer id);
+    /**
+     * 更新一条数据(修改)
+     */
+    int UpdateBlog(Blog blog, Integer id);
+}
