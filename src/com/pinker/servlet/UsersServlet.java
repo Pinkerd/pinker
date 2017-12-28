8cda674289950eccd157d59966cd27114d7d6ded
AriesGu <ff978043238@qq.com>
2017-12-28 11:56:05 +0800

    logIn && regist done. Manager delete user         search by id         search by name         show user list.done

--- src/com/pinker/servlet/UsersServlet.java
+++ src/com/pinker/servlet/UsersServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Aries.Gu on 2017/12/26.
@@ -54,18 +56,52 @@ public class UsersServlet extends BaseServlet {
     }
     /*根据id查询用户*/
     protected void findId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("jump into findId...");
 
+        String num = request.getParameter("byId");
+        Integer byId = Integer.valueOf(num);
+        System.out.println("获取到的id值"+byId);
+        pk_user byUserId = usi.findByUserId(byId);
+        ArrayList<pk_user> list=new ArrayList<pk_user>();
+        list.add(byUserId);
+        System.out.println("根据id查询的结果"+list);
+
+        request.setAttribute("userlist",list);
+        request.getRequestDispatcher("pinker/pages/userResult.jsp").forward(request,response);
     }
     /*根据姓名查询用户*/
     protected void findName(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("jump into findName...");
 
+        String byName = request.getParameter("byName");
+        System.out.println("获取到的name值"+byName);
+        List<pk_user> list = usi.findByUserName(byName);
+        System.out.println("根据name查询的结果"+list);
+
+        request.setAttribute("userlist",list);
+        request.getRequestDispatcher("pinker/pages/userResult.jsp").forward(request,response);
     }
     /*显示列表 查询所有用户*/
     protected void findAll(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("jump into findAll...");
 
+        List<pk_user> all = usi.all();
+        System.out.println(all);
+        request.setAttribute("userlist",all);
+        request.getRequestDispatcher("pinker/pages/userManager.jsp").forward(request,response);
     }
     /*根据id删除用户*/
     protected void deleteUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("jump into deleteUser...");
+
+        String num = request.getParameter("id");
+        Integer id = Integer.valueOf(num);
+        System.out.println(id);
+        boolean delete = usi.delete(id);
+
+        List<pk_user> all = usi.all();
+        request.setAttribute("userlist",all);
+        request.getRequestDispatcher("pinker/pages/userManager.jsp").forward(request,response);
 
     }
 }
